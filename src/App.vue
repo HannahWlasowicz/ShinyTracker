03af27f88bd580b0daa483b93061a540fb2afcb3
Hannah <hannahwl@buffalo.edu>
2021-08-24 20:52:08 -0400

    Renamed file

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,22 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld/>
+<!-- <div>
+  <b-tabs content-class="mt-3">
+    <b-tab title="First" active><p>I'm the first tab</p></b-tab>
+    <b-tab title="Second"><p>I'm the second tab</p></b-tab>
+    <b-tab title="Disabled" disabled><p>I'm a disabled tab!</p></b-tab>
+  </b-tabs>
+</div> -->
+  <Tracker/>
+  <Setting/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Tracker from './components/Tracker.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Tracker
   }
 }
 </script>
--- src/components/Tracker.vue
+++ src/components/Tracker.vue
@@ -9,7 +9,7 @@
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: 'Tracker',
   data() {
     return {
       count: 0
