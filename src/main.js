eacc63bb4e8317b5a9fc65f0f9a7bf8f7cfd38d2
Hannah <hannahwl@buffalo.edu>
2021-08-25 13:31:47 -0400

    adding bootstrap

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-Vue.config.productionTip = false
+
+import { BootstrapVue } from 'bootstrap-vue'
+
+import './app.scss'
+
+Vue.use(BootstrapVue)
+
 
 new Vue({
   render: h => h(App),
