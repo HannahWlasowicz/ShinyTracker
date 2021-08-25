4e1b0080c0ea9675841b53d83b75b55d1b8c93b7
Hannah <hannahwl@buffalo.edu>
2021-08-25 12:40:14 -0400

    added bootstrap, now need to change version

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,29 @@
 <template>
-<!-- <div>
-  <b-tabs content-class="mt-3">
-    <b-tab title="First" active><p>I'm the first tab</p></b-tab>
-    <b-tab title="Second"><p>I'm the second tab</p></b-tab>
-    <b-tab title="Disabled" disabled><p>I'm a disabled tab!</p></b-tab>
-  </b-tabs>
-</div> -->
+<div>
+  <b-card no-body>
+    <b-tabs card>
+      <b-tab title="Tab 1" active>
+        <b-card-text>Tab contents 1</b-card-text>
+      </b-tab>
+      <b-tab title="Tab 2">
+        <b-card-text>Tab contents 2</b-card-text>
+      </b-tab>
+    </b-tabs>
+  </b-card>
+</div>
   <Tracker/>
   <Setting/>
 </template>
 
 <script>
 import Tracker from './components/Tracker.vue'
+import Setting from './components/Settings.vue'
 
 export default {
   name: 'App',
   components: {
-    Tracker
+    Tracker,
+    Setting
   }
 }
 </script>
---
+++ src/components/Settings.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <input v-model="searh" @change="searchPokemon" type="text" placeholder="Search..">
+    </div>
+</template>
+
+<script>
+
+
+export default {
+    name: 'Settings',
+    data() {
+    return {
+      search: ''
+    }
+  },
+  methods: {
+      searchPokemon(){
+        //   Search api for pokemon
+        console.log("Sup")
+      }
+  }
+}
+</script>
